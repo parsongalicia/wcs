591f0ac92c97c38a91f7c7f772a2dc7bb5649e05
parsongalicia <34287182+parsongalicia@users.noreply.github.com>
2017-12-29 22:14:22 -0800

    Delete biggerBetterFavoriteNumber.rb

--- ch05/biggerBetterFavoriteNumber.rb
+++
@@ -1,4 +0,0 @@
-puts "Hey you! What\'s your favorite number?"
-number = gets
-puts "Really? But " + number.chomp + " is not nearly big enough to be a favorite."
-puts "I think " + (number.chomp.to_i + 1).to_s + " is a way better number."
