ef40c2415f39644be30a6e551b937179d87940c3
Parson Galicia <parsongalicia@Parsons-MacBook-Pro.local>
2017-12-29 22:15:15 -0800

    Ch05

--- ch05/biggerBetterFavoriteNumber.rb
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -1,4 +1,4 @@
 puts "Hey you! What\'s your favorite number?"
-number = gets
-puts "Really? But " + number.chomp + " is not nearly big enough to be a favorite."
-puts "I think " + (number.chomp.to_i + 1).to_s + " is a way better number."
+number = gets.chomp
+puts "Really? But " + number + " is not nearly big enough to be a favorite."
+puts "I think " + (number.to_i + 1).to_s + " is a way better number."
